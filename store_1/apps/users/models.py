6abab093af3541a8b6f5b0e74590bc40f04fe51d
Sicheng LUO <lscheng8@bu.edu>
2024-01-07 16:29:20 -0500

    'User'

--- store_1/apps/users/models.py
+++ store_1/apps/users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
 # Create your models here.
+from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
+class User(AbstractUser):
+    mobile=models.CharField(max_length=11,unique=True)
+    class Meta:
+        db_table='tb_user'
+        verbose_name='user management'
+    def __str__(self):
+        return self.username
